14bc41f1939e8b0b9352ec8029c188bb61081997
daniele123450 <41233174+daniele123450@users.noreply.github.com>
2022-01-17 17:33:46 +0100

    main

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: 'melanie!',
+        token: '',
+        playing: 'Melanie is too hot to handle'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
